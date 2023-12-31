fa4448930e98c429436ca2b8a07e3a5a44f26db9
bouanani-soufiane <soufian.bouanani2013@gmail.com>
2023-12-31 13:23:53 +0100

    finished routing

--- app/controllers/home.php
+++ app/controllers/home.php
@@ -1,7 +1,10 @@
 <?php
 class Home extends Controller{
     public function index ($name = ""){
-        echo 'home/index';
+        $user =  $this->model('User');
+        $user->name = $name;
+//        echo $user->name;
+        $this->view('home/index', ["name" => $user->name]);
     }
 
 }
